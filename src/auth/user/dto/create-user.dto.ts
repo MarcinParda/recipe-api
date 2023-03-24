e98878fec8ec68160b40910c654787bea67f2c41
MarcinParda <marcin98parda@gmail.com>
2023-03-24 15:47:59 +0100

    feat: created match custom decorator

--- src/auth/user/dto/create-user.dto.ts
+++ src/auth/user/dto/create-user.dto.ts
@@ -1,4 +1,10 @@
 import { OmitType } from '@nestjs/mapped-types';
+import { IsString } from 'class-validator';
+import { Match } from 'src/decorators/match.decorators';
 import { UpdateUserDTO } from './update-user.dto';
 
-export class CreateUsertDTO extends OmitType(UpdateUserDTO, ['id'] as const) {}
+export class CreateUsertDTO extends OmitType(UpdateUserDTO, ['id'] as const) {
+  @IsString()
+  @Match<CreateUsertDTO>('password')
+  confirmPassword: string;
+}
